ae74a6c27b40dd5227d55c702a440692b714ce4d
Rustambek03 <abdyldaevrustambek03@gmail.com>
2020-12-22 11:00:02 +0600

    fixed product details

--- src/containers/Product/ProductDetails.js
+++ src/containers/Product/ProductDetails.js
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { productsContext } from '../../contexts/ProductContextProvider';
 import { Grid, Paper } from '@material-ui/core';
 
 const ProductDetails = (props) => {
 
     const { getProductDetails, productsDetail } = useContext(productsContext)
+
+    console.log(props)
 
     useEffect(() => {
         getProductDetails(props.match.params.id)
@@ -14,40 +16,43 @@ const ProductDetails = (props) => {
 
     return (
         <div>
-            <Grid container>
-                <Grid item md={9}>
-                    <Paper>
-                        <Grid item md={9}>
-                            <Paper style={{ textAlign: "center" }}>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>Title:</td>
-                                            <td>{productsDetail.title}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Price:</td>
-                                            <td>{productsDetail.price}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Description</td>
-                                            <td> {productsDetail.description} </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Structure</td>
-                                            <td> {productsDetail.structure} </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Size</td>
-                                            <td> {productsDetail.size} </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </Paper>
-                        </Grid>
-                    </Paper>
+            {productsDetail ?
+                <Grid container>
+                    <Grid item md={9}>
+                        <Paper>
+                            <Grid item md={9}>
+                                <Paper style={{ textAlign: "center" }}>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td>Title:</td>
+                                                <td>{productsDetail.title}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Price:</td>
+                                                <td>{productsDetail.price}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Description</td>
+                                                <td> {productsDetail.description} </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Structure</td>
+                                                <td> {productsDetail.structure} </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Size</td>
+                                                <td> {productsDetail.id} </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </Paper>
+                            </Grid>
+                        </Paper>
+                    </Grid>
                 </Grid>
-            </Grid>
+                : <h1>Loading</h1>
+            }
         </div>
     );
 };
