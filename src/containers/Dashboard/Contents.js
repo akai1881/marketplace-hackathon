ac3f72dcbe6766d06fc1b788e36bc1c901029a39
akai1881 <akaibegaliev1881@gmail.com>
2020-12-22 11:44:34 +0600

    fixed modal and auth

---
+++ src/containers/Dashboard/Contents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contents = () => {
+  return (
+    <div>
+      <div>Top Content!</div>
+    </div>
+  );
+};
+
+export default Contents;
--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -4,48 +4,53 @@ import axios from 'axios';
 export const productsContext = React.createContext();
 
 let INIT_STATE = {
-    products: [],
-    productsDetail: null
-}
+  products: [],
+  productsDetail: null,
+};
 
 const reducer = (state = INIT_STATE, action) => {
-    switch (action.type) {
-        case "GET_PRODUCT_LIST":
-            return { ...state, products: action.payload }
-        case "GET_PRODUCT_DETAILS":
-            return { ...state, productsDetail: action.payload }
-    }
-}
+  switch (action.type) {
+    case 'GET_PRODUCT_LIST':
+      return { ...state, products: action.payload };
+    case 'GET_PRODUCT_DETAILS':
+      return { ...state, productsDetail: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 const ProductsContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, INIT_STATE)
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
-    async function getProductList() {
-        let { data } = await axios(`http://localhost:8000/products`)
-        dispatch({
-            type: "GET_PRODUCT_LIST",
-            payload: data
-        })
-    }
+  async function getProductList() {
+    let { data } = await axios(`http://localhost:8000/products`);
+    dispatch({
+      type: 'GET_PRODUCT_LIST',
+      payload: data,
+    });
+  }
 
-    async function getProductDetails(id) {
-        let { data } = await axios(`http://localhost:8000/products/${id}`)
-        dispatch({
-            type: "GET_PRODUCT_DETAILS",
-            payload: data
-        })
-    }
+  async function getProductDetails(id) {
+    let { data } = await axios(`http://localhost:8000/products/${id}`);
+    dispatch({
+      type: 'GET_PRODUCT_DETAILS',
+      payload: data,
+    });
+  }
 
-    return (
-        <productsContext.Provider value={{
-            products: state.products,
-            productsDetail: state.productsDetail,
-            getProductDetails,
-            getProductList
-        }}>
-            {children}
-        </productsContext.Provider>
-    );
+  return (
+    <productsContext.Provider
+      value={{
+        products: state.products,
+        productsDetail: state.productsDetail,
+        getProductDetails,
+        getProductList,
+      }}
+    >
+      {children}
+    </productsContext.Provider>
+  );
 };
 
 export default ProductsContextProvider;
