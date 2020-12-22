1eb74e526d7c17d14d8283e3451bb798a00604c0
Rustambek03 <abdyldaevrustambek03@gmail.com>
2020-12-22 18:17:48 +0600

    fixed footer

--- src/containers/Dashboard/Footer/Footer.js
+++ src/containers/Dashboard/Footer/Footer.js
@@ -7,58 +7,60 @@ const Footer = () => {
     <div className="footer-container">
       <div className="footer-content">
         <div className="footer-menu">
-          <ul>
-            <li>Все товары</li>
-            <li>Футболки</li>
-            <li>Свитшоты</li>
-            <li>Худи</li>
-            <li>Рубашки</li>
-            <li>Кепки</li>
-            <li>Шапки</li>
-            <li>Поло</li>
-            <li>Рюкзаки</li>
-            <li>Сувениры</li>
+          <ul className="footer-menu-ul">
+            <li className="footer-menu-li" >Все товары</li>
+            <li className="footer-menu-li"  >Футболки</li>
+            <li className="footer-menu-li" >Свитшоты</li>
+            <li className="footer-menu-li" >Худи</li>
+            <li className="footer-menu-li" >Рубашки</li>
+            <li className="footer-menu-li" >Кепки</li>
+            <li className="footer-menu-li" >Шапки</li>
+            <li className="footer-menu-li" >Поло</li>
+            <li className="footer-menu-li" >Рюкзаки</li>
+            <li className="footer-menu-li" >Сувениры</li>
           </ul>
         </div>
         <Grid container>
           <Grid item md={6} className="footer-left">
             <div className="footer-left-content">
-              <h1 className="footer-left-title">JOLY.BELL</h1>
-              <img
-                style={{ width: '40px', height: '40px' }}
-                src="https://image.flaticon.com/icons/png/512/36/36917.png"
-                alt=""
-              />
-              <img
-                style={{ width: '40px', height: '40px' }}
-                src="https://image.flaticon.com/icons/png/512/25/25425.png"
-                alt=""
-              />
+              <h1 style={{ color: "white" }}>JOLY.BELL</h1>
+              <div className="footer-socials">
+                <img
+                  style={{ width: '40px', height: '40px' }}
+                  src="https://image.flaticon.com/icons/png/512/36/36917.png"
+                  alt=""
+                />
+                <img
+                  style={{ width: '40px', height: '40px' }}
+                  src="https://image.flaticon.com/icons/png/512/25/25425.png"
+                  alt=""
+                />
+              </div>
             </div>
             <div>
-              <p>© 2020, JolyBell.com</p>
+              <p style={{ color: "white" }}>© 2020, JolyBell.com</p>
             </div>
           </Grid>
           <Grid item md={6} className="footer-right">
             <div className="footer-right-content">
               <div className="footer-right-email">
-                <p>E-MAIL:</p>
+                <p style={{ color: "white" }}>E-MAIL:</p>
                 <a href="">jolybellstore@gmail.com</a>
               </div>
               <div className="footer-right-working-hours">
-                <p>ВРЕМЯ РАБОТЫ СЛУЖБЫ ПОДДЕРЖКИ:</p>
-                <p>ПН-ПТ: 9:00 - 21:00</p>
+                <p style={{ color: "white" }}>ВРЕМЯ РАБОТЫ СЛУЖБЫ ПОДДЕРЖКИ:</p>
+                <p style={{ color: "white" }}>ПН-ПТ: 9:00 - 21:00</p>
               </div>
             </div>
             <div className="footer-right-payment-options">
-              <p>ПРИНИМАЕМ К ОПЛАТЕ:</p>
+              <p style={{ color: "white" }}>ПРИНИМАЕМ К ОПЛАТЕ:</p>
               <img src="" alt="" />
               <img src="" alt="" />
             </div>
           </Grid>
         </Grid>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <p>
+          <p style={{ color: "white" }}>
             Доставка по России осуществляется службами доставки: «Почта России».{' '}
             <br />
             «Почта России» осуществляет доставку до почтового отделения, индекс
@@ -66,7 +68,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/containers/Product/ProductCard.js
+++ src/containers/Product/ProductCard.js
@@ -1,10 +1,16 @@
+import { Grid, Paper } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
     return (
-        <div>
-
-        </div>
+        <Grid item xs={4}>
+            <Paper style={{ height: '200px' }}>
+                <Link to={`/details/${item.id}`} key={item.id}>
+                    <h1 style={{ color: 'black' }}>{item.title}</h1>
+                </Link>
+            </Paper>
+        </Grid>
     );
 };
 
