a5c16ef91526891090ddd4d8d5c66d63c712c97a
Rustambek03 <abdyldaevrustambek03@gmail.com>
2020-12-22 18:31:40 +0600

    s

--- src/containers/Product/ProductCard.js
+++ src/containers/Product/ProductCard.js
@@ -3,17 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ProductCard = ({ item }) => {
-<<<<<<< HEAD
-    return (
-        <Grid item xs={4}>
-            <Paper style={{ height: '200px' }}>
-                <Link to={`/details/${item.id}`} key={item.id}>
-                    <h1 style={{ color: 'black' }}>{item.title}</h1>
-                </Link>
-            </Paper>
-        </Grid>
-    );
-=======
   return (
     <Grid item xs={4}>
       <Paper style={{ height: '200px' }}>
@@ -23,7 +12,6 @@ const ProductCard = ({ item }) => {
       </Paper>
     </Grid>
   );
->>>>>>> master
 };
 
 export default ProductCard;
--- src/containers/Product/ProductList.js
+++ src/containers/Product/ProductList.js
@@ -2,25 +2,11 @@ import React, { useContext, useEffect, useState } from 'react';
 import { productsContext } from '../../contexts/ProductContextProvider';
 import { Grid, Paper } from '@material-ui/core';
 import ProductCard from './ProductCard';
-<<<<<<< HEAD
-// import PaginationComp from '../../components/Pagination';
-=======
 import PaginationComp from '../../components/Pagination';
->>>>>>> master
 
 const ProductList = () => {
   const { getProductList, products } = useContext(productsContext);
 
-<<<<<<< HEAD
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const [postPerPage] = useState(9);
-
-  // const indexOfLastPost = currentPage * postPerPage;
-  // const indexOfFirstPost = indexOfLastPost - postPerPage;
-  // const currentPost = products.slice(indexOfFirstPost, indexOfLastPost);
-
-  // const changePage = (num) => setCurrentPage(num);
-=======
   const [currentPage, setCurrentPage] = useState(1);
   const [postPerPage] = useState(9);
 
@@ -29,7 +15,6 @@ const ProductList = () => {
   const currentPost = products.slice(indexOfFirstPost, indexOfLastPost);
 
   const changePage = (num) => setCurrentPage(num);
->>>>>>> master
 
   useEffect(() => {
     getProductList();
@@ -42,19 +27,11 @@ const ProductList = () => {
           <ProductCard item={item} key={item.id} />
         ))}
       </Grid>
-<<<<<<< HEAD
-      {/* <PaginationComp
-        totalPage={products.length}
-        postPerPage={postPerPage}
-        changePage={changePage}
-      /> */}
-=======
       <PaginationComp
         totalPage={products.length}
         postPerPage={postPerPage}
         changePage={changePage}
       />
->>>>>>> master
     </>
   );
 };
